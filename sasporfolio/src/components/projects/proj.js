3ba17ef8a25e9b9d6fb8d85a50928643eb33bd5d
Sammie Steohenson <sammiestephenson@MacBook-Pro.attlocal.net>
2022-08-30 13:45:36 -0400

    Placeholders

--- sasporfolio/src/components/projects/proj.js
+++ sasporfolio/src/components/projects/proj.js
@@ -3,22 +3,36 @@ import "./proj.css";
 
 const Projects = () => {
     return (
-        <>
+        <div className="proj">
+            <h1>Projects</h1>
             <div className="netflix">
-                <img src="./assets/netPic.png" alt="" />
+                <div className="left">
+                    <div className="card">
+                        <div className="netpic">
+                            <img src="./assets/netPic.png" alt="" className="img" />
+                        </div>
+                    </div>
+                </div>
+                <div className="right">
+                    <a href="https://main.d1zo0b1ozl1ups.amplifyapp.com/GALLERY">
+                        <img src="./assets/netgitpic.png" alt="" className="img" />
+                    </a>
+                </div>
             </div>
             <div className="ecom">
                 <div className="caro">
                 </div>
             </div>
             <div className="task">
-                <div className="picture">
-                    <a herf="https://main.d26a2fuqh84ysm.amplifyapp.com/">
-                        <img src="./assets/taskday.png" alt="" />
-                    </a>
+                <div className="card">
+
+                    <div className="taskpic">
+                        <a herf="https://main.d26a2fuqh84ysm.amplifyapp.com/" />
+                    </div>
+
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
