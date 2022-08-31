a1f078006f940802fd13014529fe0603237e2879
Sammie Steohenson <sammiestephenson@MacBook-Pro.attlocal.net>
2022-08-31 09:38:21 -0400

    added text

--- sasporfolio/src/components/about/about.js
+++ sasporfolio/src/components/about/about.js
@@ -10,13 +10,17 @@ const About = () => {
                 </div>
             </div>
             <div className="abtext">
-                <h2 className="abtitle">It's nice to meet you digitally... Let me tell you a little bit about myself!</h2>
-                <p className="absub">Hello!</p>
+                <h2 className="abtitle">It's nice to meet you... Let me tell you a little bit about myself!</h2>
+                <h3 className="absub">My name is Sammie A Stephenson, or Sam for short.</h3>
                 <p className="abdesc">
-                    I'm a young, Organized, and dependable junior software engineer who
-                    can successfully manage multiple priorities. A team player willing to
-                    take on added responsibilities to drive team goals forward. I also
-                    Love to play music with my guitar!
+                    I am a young and hungry software developer who is interested in
+                    expanding my knowledge base, collaborating with others, and experiencing
+                    first-hand opportunities for growth. Well-rounded understanding of people and a strong start to my working life network. I jump at the
+                    opportunity to connect and assist others, and also face challenging situations with tact and
+                    professionalism. I look forward to bringing a fresh unique perspective to the corporate
+                    world that is comfortable bridging both veteran workers as well as the new generation
+                    coming soon to the workforce.  While my passion is bold my ability to drive results as well
+                    as be an accountable team player set me above the rest.
                 </p>
             </div>
         </div>
